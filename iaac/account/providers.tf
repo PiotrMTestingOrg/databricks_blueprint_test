3379a139e2a723cfaacbac87c3574d640ee2a312
swopmichalak <piotr.michalak@softwareone.com>
2025-09-03 20:08:45 +0200

    add param file for account

--- iaac/account/providers.tf
+++ iaac/account/providers.tf
@@ -4,3 +4,9 @@ provider "databricks" {
   account_id      = var.dbx_account_id
   azure_tenant_id = data.azurerm_client_config.current.tenant_id
 }
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = var.subscription_id
+}
--- iaac/account/variables.tf
+++ iaac/account/variables.tf
@@ -9,3 +9,7 @@ variable "region" {
 variable "admin_group" {
   description = "Name of the admin group for the project."
 }
+
+variable "subscription_id" {
+  description = "The subscription ID where the resources will be created"
+}
